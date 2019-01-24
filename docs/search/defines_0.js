e67d523fa7a95e7a8948ac20509da2d2aec264a9
Skye Soss <skyler.soss@gmail.com>
2019-01-24 18:49:37 -0500

    Added Doxygen docs

---
+++ docs/search/defines_0.js
@@ -0,0 +1,20 @@
+var searchData=
+[
+  ['ab_5fvec',['AB_vec',['../AB__vector_8h.html#ab94e5f8caca37f4c3549dda93a79b833',1,'AB_vector.h']]],
+  ['ab_5fvec_5fassert',['AB_VEC_ASSERT',['../AB__vector_8h.html#a30bc6c74e3fdf5812da81452de57b919',1,'AB_vector.h']]],
+  ['ab_5fvec_5fat',['AB_vec_at',['../AB__vector_8h.html#ac173d7525274ca88da6d492c1f85b024',1,'AB_vector.h']]],
+  ['ab_5fvec_5fcopy',['AB_vec_copy',['../AB__vector_8h.html#ac8fc28c132008df66ca29bacbfc72a2e',1,'AB_vector.h']]],
+  ['ab_5fvec_5fdestroy',['AB_vec_destroy',['../AB__vector_8h.html#a89c4bc2d8a7251f95724a0a9be7309f8',1,'AB_vector.h']]],
+  ['ab_5fvec_5ffree',['AB_VEC_FREE',['../AB__vector_8h.html#a281b2fd7622541cf9ccf946dd6740476',1,'AB_vector.h']]],
+  ['ab_5fvec_5finit',['AB_vec_init',['../AB__vector_8h.html#a36428a144ac60f8e6ac0939a9aff14b7',1,'AB_vec_init():&#160;AB_vector.h'],['../AB__vector_8h.html#ace485b4d4210a80fe6c6fae498230e5d',1,'AB_VEC_INIT():&#160;AB_vector.h']]],
+  ['ab_5fvec_5finline',['AB_VEC_INLINE',['../AB__vector_8h.html#a87642266b3a15016bc143d671a318b2e',1,'AB_vector.h']]],
+  ['ab_5fvec_5finsert',['AB_vec_insert',['../AB__vector_8h.html#a3ae223f8471ba3faf3b84355d6c94530',1,'AB_vector.h']]],
+  ['ab_5fvec_5fmax',['AB_vec_max',['../AB__vector_8h.html#aab23ee5b3cc8056277fc37bb2ad6298a',1,'AB_vector.h']]],
+  ['ab_5fvec_5fpop',['AB_vec_pop',['../AB__vector_8h.html#add30d1daaba52568e063341e91efb53e',1,'AB_vector.h']]],
+  ['ab_5fvec_5fpush',['AB_vec_push',['../AB__vector_8h.html#a0df1ab0cadbb272a92adebd014c24fcb',1,'AB_vector.h']]],
+  ['ab_5fvec_5fpushp',['AB_vec_pushp',['../AB__vector_8h.html#a6f8174860c8ace270e2275d4ee4477d6',1,'AB_vector.h']]],
+  ['ab_5fvec_5frealloc',['AB_VEC_REALLOC',['../AB__vector_8h.html#a14045a6a84985e4e78041ec906a6f928',1,'AB_vector.h']]],
+  ['ab_5fvec_5fresize',['AB_vec_resize',['../AB__vector_8h.html#a2bc8598959bf709406e23a78c01673aa',1,'AB_vector.h']]],
+  ['ab_5fvec_5fsize',['AB_vec_size',['../AB__vector_8h.html#ac58d49b2da9a94bf7e29925d54b82e0c',1,'AB_vector.h']]],
+  ['ab_5fvec_5fstatic_5fassert',['AB_VEC_STATIC_ASSERT',['../AB__vector_8h.html#aba926c654c7e135e80983ad0218e34f0',1,'AB_vector.h']]]
+];
---
+++ docs/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['ab_5fvector_2eh',['AB_vector.h',['../AB__vector_8h.html',1,'']]]
+];
